ea41b928368092cf6d5fd1d84e63cfbefecf4a70
ycz <yuchangzhou@gmail.com>
2023-01-14 21:42:05 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('next').NextConfig} */
+/*const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig*/
+
+// next.config.js
+const semi = require('@douyinfe/semi-next').default({
+    /* the extension options */
+});
+module.exports = semi({
+    // your custom Next.js configuration
+});
